35868401d8860bf4ffd7b81cc36c92da274aa207
Prajwal Thapa <prajwal.thapa.986@gmail.com>
2022-11-04 23:11:17 +0545

    Verify Mail Update

--- lib/Firebase/verify_mail.dart
+++ lib/Firebase/verify_mail.dart
@@ -61,23 +61,45 @@ class _VerifyMailPageState extends State<VerifyMailPage> {
   Widget build(BuildContext context) => isEmailVerified
       ? const HomePage()
       : Scaffold(
+    backgroundColor: Colors.grey[300],
+    appBar: AppBar(
+      backgroundColor: Colors.deepPurple,
+      leading: const Icon(Icons.email_rounded),
+      title: const Text('Verify Mail'),
+    ),
     body: Padding(
-      padding: const EdgeInsets.all(16),
+      padding: const EdgeInsets.all(20),
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          const Text(
-            'Please Check Provided Email!!',
-            style: TextStyle(
-                fontFamily: 'OpenSans', fontWeight: FontWeight.bold),
+          const Center(
+            child: Text(
+              'Please Check Your Email',
+              style: TextStyle(
+                  fontFamily: 'BebasNeue',
+                  fontSize: 24
+              ),
+            ),
           ),
+
+          const SizedBox(height: 10),
+
           const Text(
-            'A verification email has been sent to your email!!!',
+            'A verification email has been sent',
             style: TextStyle(
-                fontFamily: 'OpenSans', fontWeight: FontWeight.bold),
+                fontFamily: 'BebasNeue',
+              fontSize: 20
+               ),
           ),
           const SizedBox(height: 24),
+
           ElevatedButton.icon(
+              style: ElevatedButton.styleFrom(
+                  primary: Colors.deepPurple,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10)
+                  )
+              ),
               onPressed: (){sendVerificationEmail();},
               icon: const Icon(
                 Icons.email_rounded,
@@ -86,15 +108,30 @@ class _VerifyMailPageState extends State<VerifyMailPage> {
               label: const Text(
                 'Resend Email',
                 style: TextStyle(
-                    fontFamily: 'OpenSans', fontWeight: FontWeight.bold),
-              )),
-          TextButton(onPressed: () =>FirebaseAuth.instance.signOut().then((value) {
+                    fontFamily: 'BebasNeue',
+                    fontWeight: FontWeight.bold
+                ),
+              )
+          ),
+
+          TextButton(
+              style: TextButton.styleFrom(
+                  primary: Colors.deepPurple
+              ),
+              onPressed: () =>FirebaseAuth.instance.signOut().then((value) {
             Navigator.push(
                 context,
                 MaterialPageRoute(builder: (context)=> const LoginPage()
                 )
             );
-          }), child: const Text('Cancel'))
+          }), child: const Text(
+              'Cancel',
+            style: TextStyle(
+                decoration: TextDecoration.underline,
+                decorationThickness: 1
+            ),
+          )
+          )
         ],
       ),
     ),
