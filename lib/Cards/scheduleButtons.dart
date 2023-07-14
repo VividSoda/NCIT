8d20ad2155954518f68b3b01a2f71a074b1ad339
VividSoda <prajwal.thapa.986@gmail.com>
2023-07-15 02:05:24 +0545

    Indentation

--- lib/Cards/scheduleButtons.dart
+++ lib/Cards/scheduleButtons.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:umbrella_care/Constants/colors.dart';
 import 'package:umbrella_care/Models/buttonModel.dart';
+
 class ScheduleButtons extends StatelessWidget {
   final ButtonModel button;
   final VoidCallback onPressed;
-  const ScheduleButtons({Key? key, required this.button, required this.onPressed}) : super(key: key);
+
+  const ScheduleButtons(
+      {Key? key, required this.button, required this.onPressed})
+      : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -14,21 +19,18 @@ class ScheduleButtons extends StatelessWidget {
         child: ElevatedButton(
             onPressed: onPressed,
             style: ElevatedButton.styleFrom(
-              backgroundColor: button.isSelected? primary : fadedPrimary,
+                backgroundColor: button.isSelected ? primary : fadedPrimary,
                 shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(25)
-                )
-            ),
+                    borderRadius: BorderRadius.circular(25))),
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 Text(
                   button.day,
                   style: const TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.w800,
-                    color: Colors.white
-                  ),
+                      fontSize: 20,
+                      fontWeight: FontWeight.w800,
+                      color: Colors.white),
                 ),
                 const SizedBox(height: 10),
                 Text(
@@ -36,12 +38,10 @@ class ScheduleButtons extends StatelessWidget {
                   style: const TextStyle(
                       fontSize: 14,
                       fontWeight: FontWeight.w600,
-                      color: Colors.white
-                  ),
+                      color: Colors.white),
                 ),
               ],
-            )
-        ),
+            )),
       ),
     );
   }
