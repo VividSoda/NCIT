4a83ece5917c7f6d76c231a7eb63c62f57ad35a6
VividSoda <prajwal.thapa.986@gmail.com>
2023-07-15 01:57:09 +0545

    Colors

--- lib/Constants/colors.dart
+++ lib/Constants/colors.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-
 const Color primary = Color(0xFF5E1A84);
 const Color greyBorders = Color(0xFFD7DEEA);
 const Color fadedPrimary = Color.fromRGBO(94, 26, 132, 0.6);
