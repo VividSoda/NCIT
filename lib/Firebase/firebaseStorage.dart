e8fab73e092b3e6d8b9d9aeb7f58c0f295610d9c
VividSoda <prajwal.thapa.986@gmail.com>
2023-07-15 07:11:05 +0545

    Firebase

--- lib/Firebase/firebaseStorage.dart
+++ lib/Firebase/firebaseStorage.dart
@@ -1,159 +1,121 @@
 import 'dart:io';
-
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 
-class FirebaseApi{
-  static UploadTask? uploadFile(String destination, File file){
-    try{
+class FirebaseApi {
+  static UploadTask? uploadFile(String destination, File file) {
+    try {
       final ref = FirebaseStorage.instance.ref(destination);
 
       return ref.putFile(file);
-    }
-
-    on FirebaseException catch(e){
+    } on FirebaseException catch (e) {
       return null;
     }
   }
 
-  //Add records data to firebase
-  static Future submitRecords(String docId, String patientId, String recordPath, String reportName) async{
+  static Future submitRecords(String docId, String patientId, String recordPath,
+      String reportName) async {
     DateTime dateTime = DateTime.now();
     final db = FirebaseFirestore.instance;
-
-    return db.collection('records').add(
-        {
-          'doctor id' : docId,
-          'patient id' : patientId,
-          'record path' : recordPath,
-          'date created' : dateTime.toString(),
-          'name' : reportName
-        }
-    ).then((value){
+    return db.collection('records').add({
+      'doctor id': docId,
+      'patient id': patientId,
+      'record path': recordPath,
+      'date created': dateTime.toString(),
+      'name': reportName
+    }).then((value) {
       addRecordsToDoctor(docId, patientId, value.id);
       addRecordsToPatient(docId, patientId, value.id);
     });
   }
 
-  //add record info to doctor
-  static Future addRecordsToDoctor(String docId, String patientId, String recordId) async{
-    print('doctor++++++++++++++++++++++++');
+  static Future addRecordsToDoctor(
+      String docId, String patientId, String recordId) async {
     List<String> recordIds = [];
-    final docRecord = FirebaseFirestore.instance.collection('doctors').doc(docId).collection('records').doc(patientId);
-
+    final docRecord = FirebaseFirestore.instance
+        .collection('doctors')
+        .doc(docId)
+        .collection('records')
+        .doc(patientId);
     DocumentSnapshot<Map<String, dynamic>> snapshot = await docRecord.get();
-
-    if(snapshot.exists){
+    if (snapshot.exists) {
       Map<String, dynamic> data = snapshot.data()!;
-
       List<dynamic> list = data['record ids'];
-
       recordIds = list.cast<String>();
-
       recordIds.add(recordId);
-
-      docRecord.update(
-          {
-            'record ids' : recordIds
-          }
-      );
-    }
-
-    else{
+      docRecord.update({'record ids': recordIds});
+    } else {
       DateTime dateTime = DateTime.now();
       recordIds.add(recordId);
 
       docRecord.set({
-        'date created' : dateTime.toString(),
-        'patient id' : patientId,
-        'record ids' :recordIds
+        'date created': dateTime.toString(),
+        'patient id': patientId,
+        'record ids': recordIds
       });
     }
   }
 
-  //add record info to patient
-  static Future addRecordsToPatient(String docId, String patientId, String recordId) async{
-    print('patient++++++++++++++++++++++++');
+  static Future addRecordsToPatient(
+      String docId, String patientId, String recordId) async {
     List<String> recordIds = [];
-    final patientRecord = FirebaseFirestore.instance.collection('patients').doc(patientId).collection('records').doc(docId);
-
+    final patientRecord = FirebaseFirestore.instance
+        .collection('patients')
+        .doc(patientId)
+        .collection('records')
+        .doc(docId);
     DocumentSnapshot<Map<String, dynamic>> snapshot = await patientRecord.get();
-
-    if(snapshot.exists){
+    if (snapshot.exists) {
       Map<String, dynamic> data = snapshot.data()!;
-
       List<dynamic> list = data['record ids'];
-
       recordIds = list.cast<String>();
-
       recordIds.add(recordId);
-
-      patientRecord.update(
-          {
-            'record ids' : recordIds
-          }
-      );
-    }
-
-    else{
+      patientRecord.update({'record ids': recordIds});
+    } else {
       DateTime dateTime = DateTime.now();
       recordIds.add(recordId);
-
       patientRecord.set({
-        'date created' : dateTime.toString(),
-        'doctor id' : docId,
-        'record ids' : recordIds
+        'date created': dateTime.toString(),
+        'doctor id': docId,
+        'record ids': recordIds
       });
     }
   }
 
-  //Add records data to firebase when patient themselves upload
-  static Future submitRecordsSelf(String patientId, String recordPath) async{
+  static Future submitRecordsSelf(String patientId, String recordPath) async {
     DateTime dateTime = DateTime.now();
     final db = FirebaseFirestore.instance;
-
-    return db.collection('records').add(
-        {
-          'patient id' : patientId,
-          'record path' : recordPath,
-          'date created' : dateTime.toString()
-        }
-    ).then((value){
+    return db.collection('records').add({
+      'patient id': patientId,
+      'record path': recordPath,
+      'date created': dateTime.toString()
+    }).then((value) {
       addRecordsToPatientSelf(patientId, value.id);
     });
   }
 
-  //add record info to patient on self upload
-  static Future addRecordsToPatientSelf(String patientId, String recordId) async{
+  static Future addRecordsToPatientSelf(
+      String patientId, String recordId) async {
     List<String> recordIds = [];
-    final patientRecord = FirebaseFirestore.instance.collection('patients').doc(patientId).collection('self uploaded records').doc(patientId);
-
+    final patientRecord = FirebaseFirestore.instance
+        .collection('patients')
+        .doc(patientId)
+        .collection('self uploaded records')
+        .doc(patientId);
     DocumentSnapshot<Map<String, dynamic>> snapshot = await patientRecord.get();
-
-    if(snapshot.exists){
+    if (snapshot.exists) {
       Map<String, dynamic> data = snapshot.data()!;
-
       List<dynamic> list = data['record ids'];
-
       recordIds = list.cast<String>();
-
       recordIds.add(recordId);
-
-      patientRecord.update(
-          {
-            'record ids' : recordIds
-          }
-      );
-    }
-
-    else{
+      patientRecord.update({'record ids': recordIds});
+    } else {
       DateTime dateTime = DateTime.now();
       recordIds.add(recordId);
-
       patientRecord.set({
-        'date created' : dateTime.toString(),
-        'patient id' : patientId,
-        'record ids' : recordIds
+        'date created': dateTime.toString(),
+        'patient id': patientId,
+        'record ids': recordIds
       });
     }
   }
